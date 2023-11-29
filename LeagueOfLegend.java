72671ead1c9726c8da6bc94a9d0fee7ac75b29a2
ParkJoonHo <wns4090@gmail.com>
2023-11-29 15:11:56 +0900

    유진승

---
+++ LeagueOfLegend.java
@@ -0,0 +1,34 @@
+package Project;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import javax.swing.*;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class LeagueOfLegend {
+    public static void main(String[] jinseung) {
+        String Nick = JOptionPane.showInputDialog("소환사명을 입력해주세요. (ex 김도영 #2007)", "김도영#2007");
+        StringTokenizer st = new StringTokenizer(Nick, "#");
+        String url = "https://www.op.gg/summoners/kr/" + st.nextToken() + "-" + st.nextToken();
+        try {
+            Document doc = Jsoup.connect(url).get();
+            Element table = doc.selectFirst("table");
+            Element div = doc.selectFirst("div.tier");
+            Element div2 = doc.selectFirst("div.lp");
+            String result = div.html();
+            String result2 = div2.html();
+            result = result.replaceAll("<br>", "");
+            result2 = result2.replaceAll("<br>", "");
+            result = result.replaceAll("<!-- -->", "");
+            result2 = result2.replaceAll("<!-- -->", "");
+            result = result.replaceAll(" ", "");
+            result2 = result2.replaceAll(" ", "");
+            JOptionPane.showMessageDialog(null, "Tier : " + result + "\n" + "LP : " + result2, "GB.GG",1);
+        } catch (IOException e){
+            JOptionPane.showMessageDialog(null, "존재하지 않는 소환사명입니다", "GB.GG", 0);
+        }
+    }
+}
